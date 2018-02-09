2b63b268fd33d09e9f62e39f0aa1e42d1be7f59f
Eric <eric.li5009@berkeley.edu>
2018-02-09 00:04:57 -0800

    implemented start, options, pause menu and loading screen

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class LevelLoader : MonoBehaviour {
+    public GameObject loadingScreen;
+    public Slider slider;
+    public TextMeshProUGUI progressText;
+    public int nextScene;
+
+    public void Start()
+    {
+        LoadLevel(nextScene);
+    }
+
+    public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsyncronously(sceneIndex));
+    }
+
+    IEnumerator LoadAsyncronously (int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
+            progressText.text = progress * 100f + "%";
+            yield return null;
+        }
+    }
+}
